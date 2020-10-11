bab37921cf538173f52549554daa9f8616862506
CA New York <canewyorkwebsite@gmail.com>
2020-10-11 18:06:51 -0400

    Updates

---
+++ precache-manifest.ed5df9f404ab9e8471d9908b311bc958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c72012722b53bcb3c57a90d86087eb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d9d9c804b4c73136458",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "ae0321f5d191d15bdb1c",
+    "url": "./static/js/2.5d3955b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5d3955b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9d9c804b4c73136458",
+    "url": "./static/js/main.b50297f6.chunk.js"
+  },
+  {
+    "revision": "f0fafb0a4d1490635983",
+    "url": "./static/js/runtime-main.53a14f44.js"
+  }
+]);
---
+++ static/js/main.b50297f6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpca-card-example"]=this["webpackJsonpca-card-example"]||[]).push([[0],[,,function(e,n,a){e.exports=a(11)},function(e,n,a){},,,,,,,function(e,n,a){},function(e,n,a){"use strict";a.r(n);a(3);var t=a(0),r=a.n(t),c=a(1),o=a.n(c),i="_3ybTi",s=function(e){var n=e.area;return r.a.createElement("div",{className:i},"Component: ",n)},u=(a(10),()=>r.a.createElement(s,{area:"New York"}));o.a.render(r.a.createElement(u,null),document.getElementById("root"))}],[[2,1,2]]]);
+//# sourceMappingURL=main.b50297f6.chunk.js.map
